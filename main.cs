f36f9feba3a258283a454e096158e034e3aed117
Omer Khan <omerkhan.nu@gmail.com>
2022-10-14 12:32:29 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,15 @@
+// Task # 1
+// You have to write a function which takes an array of integers "seq" and an integer "target". There exist two elements of the array whose sum is equal to the target. You can assume that there exists only one solution. You have to return the indices of those elements
+// Example 1: [1,9,2,19]  Target = 3, Answer = 0,2
+
+// Example 2: [2,4]  Target = 6, Answer =0,1
+
+using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World");
+    }
+}
